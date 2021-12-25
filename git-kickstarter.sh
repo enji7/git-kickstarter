8f3117f57b145b6628040dc68b0cc3518c45ac1b
enji7 <enji.systems@gmail.com>
2021-12-25 19:58:54 +0100

    corrections

--- git-kickstarter.sh
+++ git-kickstarter.sh
@@ -1,14 +1,27 @@
-# after creating a GitHub repo (without any initial content), insert its URL here:
-GITHUB_REPO = "https://github.com/enji7/spring-demo-nodeps.git"
+# HOW TO USE THIS SCRIPT:
+# - create local project (raw, without Git)
+# - create GitHub repo (without any initial content)
+# - note: project directory name and GitHub repo name must be identical
+# - change into the project's directory
+# - execute this script from the project's directory (the script will automatically determine the project's name)
 
-# prepare local git repo
+PROJECT_NAME=`basename $PWD`
+
+# after creating a GitHub repo (without any initial content), insert its URL here:
+GITHUB_REPO="https://github.com/enji7/${PROJECT_NAME}.git"
+
+# prepare local repo
 git init
 git add .
 git commit -m "initial commit"
 
-# connect local with remote git repo
+# rename branch
+git branch -M main
+
+# connect local with remote repo
 git remote add origin $GITHUB_REPO
+# verify remote repo
+git remote -v
 
 # push from local to remote
 git push -u origin main
-
